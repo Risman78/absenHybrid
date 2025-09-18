91ed2bad6f4e6de63f53d6a94696b39ab0e8d6bf
Risman78 <rismangrabonline@gmail.com>
2025-09-18 18:27:38 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,25 @@
-const cacheName = 'absensi-pwa-v1';
-const assets = [
+const CACHE_NAME = 'absensi-cache-v1';
+const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
+  '/css/style.css',
+  '/js/app.js',
   '/assets/images/prambanan.jpg',
   '/assets/icons/icon-192.png',
   '/assets/icons/icon-512.png'
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache=>cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => res || fetch(evt.request))
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
   );
 });
-
